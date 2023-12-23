7aa14e833c6e5190d807e82e5bc9b8596b22b054
hsiaoxychen <oyiadin@gmail.com>
2023-12-23 10:47:36 +0800

    更新汇编里的注释格式，减少报错

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -1,32 +1,32 @@
-! bootsec 到时候是镜像中最打头的部分
+# bootsec 到时候是镜像中最打头的部分
 
-!
-! SYS_SIZE is the number of clicks (16 bytes) to be loaded.
-! 0x3000 is 0x30000 bytes = 196kB, more than enough for current
-! versions of linux
-!
+#
+# SYS_SIZE is the number of clicks (16 bytes) to be loaded.
+# 0x3000 is 0x30000 bytes = 196kB, more than enough for current
+# versions of linux
+#
 SYSSIZE = 0x3000
-!
-!	bootsect.s		(C) 1991 Linus Torvalds
-!
-! bootsect.s is loaded at 0x7c00 by the bios-startup routines, and moves
-! itself out of the way to address 0x90000, and jumps there.
-!
-! It then loads 'setup' directly after itself (0x90200), and the system
-! at 0x10000, using BIOS interrupts. 
-!
-! NOTE! currently system is at most 8*65536 bytes long. This should be no
-! problem, even in the future. I want to keep it simple. This 512 kB
-! kernel size should be enough, especially as this doesn't contain the
-! buffer cache as in minix
-!
-! The loader has been made as simple as possible, and continuos
-! read errors will result in a unbreakable loop. Reboot by hand. It
-! loads pretty fast by getting whole sectors at a time whenever possible.
+#
+#	bootsect.s		(C) 1991 Linus Torvalds
+#
+# bootsect.s is loaded at 0x7c00 by the bios-startup routines, and moves
+# itself out of the way to address 0x90000, and jumps there.
+#
+# It then loads 'setup' directly after itself (0x90200), and the system
+# at 0x10000, using BIOS interrupts. 
+#
+# NOTE! currently system is at most 8*65536 bytes long. This should be no
+# problem, even in the future. I want to keep it simple. This 512 kB
+# kernel size should be enough, especially as this doesn't contain the
+# buffer cache as in minix
+#
+# The loader has been made as simple as possible, and continuos
+# read errors will result in a unbreakable loop. Reboot by hand. It
+# loads pretty fast by getting whole sectors at a time whenever possible.
 
-! BIOS 会将这份文件载入到 0x7c00 处
-! 首先将自身挪到 0x90000，然后跳转过去，接着将 setup 载入到 0x90200 处
-! 载入完成后就跳转到 setup 的入口
+# BIOS 会将这份文件载入到 0x7c00 处
+# 首先将自身挪到 0x90000，然后跳转过去，接着将 setup 载入到 0x90200 处
+# 载入完成后就跳转到 setup 的入口
 
 .globl begtext, begdata, begbss, endtext, enddata, endbss
 .text
@@ -37,18 +37,24 @@ begdata:
 begbss:
 .text
 
-SETUPLEN = 4				! nr of setup-sectors
-! 下列数值是段值，所以要 <<4，得到 0x7c00
-BOOTSEG  = 0x07c0			! original address of boot-sector
-INITSEG  = 0x9000			! we move boot here - out of the way
-SETUPSEG = 0x9020			! setup starts here
-SYSSEG   = 0x1000			! system loaded at 0x10000 (65536).
-ENDSEG   = SYSSEG + SYSSIZE		! where to stop loading
+# nr of setup-sectors
+SETUPLEN = 4
+# 下列数值是段值，所以要 <<4，得到 0x7c00
+# original address of boot-sector
+BOOTSEG  = 0x07c0
+# we move boot here - out of the way
+INITSEG  = 0x9000
+# setup starts here
+SETUPSEG = 0x9020
+# system loaded at 0x10000 (65536).
+SYSSEG   = 0x1000
+# where to stop loading
+ENDSEG   = SYSSEG + SYSSIZE
 
-! ROOT_DEV:	0x000 - same type of floppy as boot.
-!		0x301 - first partition on first drive etc
+# ROOT_DEV:	0x000 - same type of floppy as boot.
+#		0x301 - first partition on first drive etc
 ROOT_DEV = 0x306
-! 会被放到本文件末尾的 root_dev 里
+# 会被放到本文件末尾的 root_dev 里
 
 entry start
 start:
@@ -57,133 +63,166 @@ start:
 	mov	ax,#INITSEG
 	mov	es,ax
 	mov	cx,#256
-	sub	si,si			! ds:si = 0x07c0:0x0000
-	sub	di,di			! es:di = 0x9000:0x0000
+	# ds:si = 0x07c0:0x0000
+	sub	si,si
+	# es:di = 0x9000:0x0000
+	sub	di,di
 	rep
-	movw				! 移动1个字，重复256次（512 Bytes）
-	jmpi	go,INITSEG	! 跳到移动后的 go 标号处（0x90000+go）
-go:	mov	ax,cs			! 重设段寄存器 ds,es,ss，重设到当前代码所在段即 0x9000
+	# 移动1个字，重复256次（512 Bytes）
+	movw
+	# 跳到移动后的 go 标号处（0x90000+go）
+	jmpi	go,INITSEG
+# 重设段寄存器 ds,es,ss，重设到当前代码所在段即 0x9000
+go:	mov	ax,cs
 	mov	ds,ax
 	mov	es,ax
-! put stack at 0x9ff00.
+# put stack at 0x9ff00.
 	mov	ss,ax
-	mov	sp,#0xFF00		! arbitrary value >>512
-! 栈随便放，不碍着地就行，Linus选了个 0x9ff00（注意栈往低处增长）
+	# arbitrary value >>512
+	mov	sp,#0xFF00
+# 栈随便放，不碍着地就行，Linus选了个 0x9ff00（注意栈往低处增长）
 
-! load the setup-sectors directly after the bootblock.
-! Note that 'es' is already set up.
+# load the setup-sectors directly after the bootblock.
+# Note that 'es' is already set up.
 
-! 注意此时 es 段寄存器是 0x9000
+# 注意此时 es 段寄存器是 0x9000
 
 load_setup:
-	mov	dx,#0x0000		! drive 0, head 0
-	mov	cx,#0x0002		! sector 2, track 0
-	mov	bx,#0x0200		! address = 512, in INITSEG
-	mov	ax,#0x0200+SETUPLEN	! service 2, nr of sectors
-	int	0x13			! read it
-	! 读取 setup 的数据，放到 es:bx 即 0x9000:0x0200 处
-	! 总共读取 4 个扇区 (SETUPLEN)
-	! int 0x13 是 BIOS 提供的服务，可以读磁盘
-	jnc	ok_load_setup		! ok - continue
+	# drive 0, head 0
 	mov	dx,#0x0000
-	mov	ax,#0x0000		! reset the diskette
-	! 如果不成功就重设一些参数后重新尝试
+	# sector 2, track 0
+	mov	cx,#0x0002
+	# address = 512, in INITSEG
+	mov	bx,#0x0200
+	# service 2, nr of sectors
+	mov	ax,#0x0200+SETUPLEN
+	# read it
+	int	0x13
+	# 读取 setup 的数据，放到 es:bx 即 0x9000:0x0200 处
+	# 总共读取 4 个扇区 (SETUPLEN)
+	# int 0x13 是 BIOS 提供的服务，可以读磁盘
+	# ok - continue
+	jnc	ok_load_setup
+	mov	dx,#0x0000
+	# reset the diskette
+	mov	ax,#0x0000
+	# 如果不成功就重设一些参数后重新尝试
 	int	0x13
 	j	load_setup
 
 ok_load_setup:
 
-! Get disk drive parameters, specifically nr of sectors/track
+# Get disk drive parameters, specifically nr of sectors/track
 
 	mov	dl,#0x00
-	mov	ax,#0x0800		! AH=8 is get drive parameters
-	int	0x13 			! 该中断服务程序会将磁盘参数表置于 es:di 处
+	# AH=8 is get drive parameters
+	mov	ax,#0x0800
+	# 该中断服务程序会将磁盘参数表置于 es:di 处
+	int	0x13
 	mov	ch,#0x00
 	seg cs
-	mov	sectors,cx 		! 保存每磁道最大扇区数到 sectors (见本文件末)
-	! 因为 sectors 就在本文件末，所以是 cs:sectors
+	# 保存每磁道最大扇区数到 sectors (见本文件末)
+	mov	sectors,cx
+	# 因为 sectors 就在本文件末，所以是 cs:sectors
 	mov	ax,#INITSEG
-	mov	es,ax 			! int 0x13 似乎会破坏 es 的值，重新改正
+	# int 0x13 似乎会破坏 es 的值，重新改正
+	mov	es,ax
 
-! Print some inane message
+# Print some inane message
 
-	mov	ah,#0x03		! read cursor pos
+    # read cursor pos
+	mov	ah,#0x03
 	xor	bh,bh
 	int	0x10
 	
 	mov	cx,#24
-	mov	bx,#0x0007		! page 0, attribute 7 (normal)
+	# page 0, attribute 7 (normal)
+	mov	bx,#0x0007
 	mov	bp,#msg1
-	mov	ax,#0x1301		! write string, move cursor
+	# write string, move cursor
+	mov	ax,#0x1301
 	int	0x10
 
-! ok, we've written the message, now
-! we want to load the system (at 0x10000)
+# ok, we've written the message, now
+# we want to load the system (at 0x10000)
 
-! 将 system 模块载入到 0x10000 处
-! system 模块其实就是 boot/head.s 跟 init/main.c
-! 具体可以看 /Makefile 文件
+# 将 system 模块载入到 0x10000 处
+# system 模块其实就是 boot/head.s 跟 init/main.c
+# 具体可以看 /Makefile 文件
 
 	mov	ax,#SYSSEG
-	mov	es,ax		! segment of 0x010000
+	# segment of 0x010000
+	mov	es,ax
 	call	read_it
-	call	kill_motor 	! TODO: 关闭软驱的电机，不过我不清楚为什么要有这一步
+	# TODO: 关闭软驱的电机，不过我不清楚为什么要有这一步
+	call	kill_motor
 
-! After that we check which root-device to use. If the device is
-! defined (!= 0), nothing is done and the given device is used.
-! Otherwise, either /dev/PS0 (2,28) or /dev/at0 (2,8), depending
-! on the number of sectors that the BIOS reports currently.
+# After that we check which root-device to use. If the device is
+# defined (!= 0), nothing is done and the given device is used.
+# Otherwise, either /dev/PS0 (2,28) or /dev/at0 (2,8), depending
+# on the number of sectors that the BIOS reports currently.
 
 	seg cs
 	mov	ax,root_dev
 	cmp	ax,#0
-	jne	root_defined 	! root_dev 定义过了，不作处理
-	seg cs 				! 否则根据每磁道扇区数判断是啥类型的磁盘
+	# root_dev 定义过了，不作处理
+	jne	root_defined
+	# 否则根据每磁道扇区数判断是啥类型的磁盘
+	seg cs
 	mov	bx,sectors
-	mov	ax,#0x0208		! /dev/ps0 - 1.2Mb
+	# /dev/ps0 - 1.2Mb
+	mov	ax,#0x0208
 	cmp	bx,#15
 	je	root_defined
-	mov	ax,#0x021c		! /dev/PS0 - 1.44Mb
+	# /dev/PS0 - 1.44Mb
+	mov	ax,#0x021c
 	cmp	bx,#18
 	je	root_defined
-undef_root: 			! 找不到就死循环
+# 找不到就死循环
+undef_root:
 	jmp undef_root
 root_defined:
 	seg cs
 	mov	root_dev,ax
 
-! after that (everyting loaded), we jump to
-! the setup-routine loaded directly after
-! the bootblock:
+# after that (everyting loaded), we jump to
+# the setup-routine loaded directly after
+# the bootblock:
 
 	jmpi	0,SETUPSEG
-	! 跳到 setup 起始处
-	! 此时 setup 已经被载入到 0x10000～0x30000 处了
+	# 跳到 setup 起始处
+	# 此时 setup 已经被载入到 0x10000～0x30000 处了
 
-! This routine loads the system at address 0x10000, making sure
-! no 64kB boundaries are crossed. We try to load it as fast as
-! possible, loading whole tracks whenever we can.
-!
-! in:	es - starting address segment (normally 0x1000)
-!
-sread:	.word 1+SETUPLEN	! sectors read of current track
-head:	.word 0			! current head
-track:	.word 0			! current track
+# This routine loads the system at address 0x10000, making sure
+# no 64kB boundaries are crossed. We try to load it as fast as
+# possible, loading whole tracks whenever we can.
+#
+# in:	es - starting address segment (normally 0x1000)
+#
+# sectors read of current track
+sread:	.word 1+SETUPLEN
+# current head
+head:	.word 0
+# current track
+track:	.word 0
 
 read_it:
 	mov ax,es
 	test ax,#0x0fff
 die:
-	jne die			! es must be at 64kB boundary
-	! 0x1000 & 0x0fff = 0, ZF=1
-	! 所以这里不会进入死循环 (ZF=0 才会死循环)
-	! 以这种方式检查 es 是否与 64KiB 对齐
-	! TODO: 不清楚为什么一定要对齐
-	xor bx,bx		! bx is starting address within segment
-	! 下边就懒得读了，意义不大，就是利用 int 0x13 读数据而已
+	# es must be at 64kB boundary
+	jne die
+	# 0x1000 & 0x0fff = 0, ZF=1
+	# 所以这里不会进入死循环 (ZF=0 才会死循环)
+	# 以这种方式检查 es 是否与 64KiB 对齐
+	# TODO: 不清楚为什么一定要对齐
+	# bx is starting address within segment
+	xor bx,bx
+	# 下边就懒得读了，意义不大，就是利用 int 0x13 读数据而已
 rp_read:
 	mov ax,es
-	cmp ax,#ENDSEG		! have we loaded all yet?
+	# have we loaded all yet?
+	cmp ax,#ENDSEG
 	jb ok1_read
 	ret
 ok1_read:
@@ -275,7 +314,7 @@ msg1:
 	.byte 13,10,13,10
 
 .org 508
-! 会被 tools/build.c 填充进真实的设备号
+# 会被 tools/build.c 填充进真实的设备号
 root_dev:
 	.word ROOT_DEV
 boot_flag:
--- boot/setup.s
+++ boot/setup.s
@@ -1,28 +1,31 @@
-!
-!	setup.s		(C) 1991 Linus Torvalds
-!
-! setup.s is responsible for getting the system data from the BIOS,
-! and putting them into the appropriate places in system memory.
-! both setup.s and system has been loaded by the bootblock.
-!
-! This code asks the bios for memory/disk/other parameters, and
-! puts them in a "safe" place: 0x90000-0x901FF, ie where the
-! boot-block used to be. It is then up to the protected mode
-! system to read them from there before the area is overwritten
-! for buffer-blocks.
-!
+# 
+# 	setup.s		(C) 1991 Linus Torvalds
+# 
+# setup.s is responsible for getting the system data from the BIOS,
+# and putting them into the appropriate places in system memory.
+# both setup.s and system has been loaded by the bootblock.
+# 
+# This code asks the bios for memory/disk/other parameters, and
+# puts them in a "safe" place: 0x90000-0x901FF, ie where the
+# boot-block used to be. It is then up to the protected mode
+# system to read them from there before the area is overwritten
+# for buffer-blocks.
+# 
 
-! 从 bootsec 跳转而来
-! 此文件此时已经被载入到内存里的 0x90200 处
-! 同时，system 模块已经被载入到 0x10000 处
-! 此文件中将向 BIOS 查询一些硬件信息，并存储到 0x90000-0x901FF
-! 这块区域曾经放置着 bootsect，但现在 bootsect 的使命已经结束了，可以拿来复用，直接覆盖
+# 从 bootsec 跳转而来
+# 此文件此时已经被载入到内存里的 0x90200 处
+# 同时，system 模块已经被载入到 0x10000 处
+# 此文件中将向 BIOS 查询一些硬件信息，并存储到 0x90000-0x901FF
+# 这块区域曾经放置着 bootsect，但现在 bootsect 的使命已经结束了，可以拿来复用，直接覆盖
 
-! NOTE! These had better be the same as in bootsect.s!
+# NOTE! These had better be the same as in bootsect.s!
 
-INITSEG  = 0x9000	! we move boot here - out of the way
-SYSSEG   = 0x1000	! system loaded at 0x10000 (65536).
-SETUPSEG = 0x9020	! this is the current segment
+# we move boot here - out of the way
+INITSEG  = 0x9000
+# system loaded at 0x10000 (65536).
+SYSSEG   = 0x1000
+# this is the current segment
+SETUPSEG = 0x9020
 
 .globl begtext, begdata, begbss, endtext, enddata, endbss
 .text
@@ -36,35 +39,41 @@ begbss:
 entry start
 start:
 
-! 下边这一系列类似的代码，作用是将一些硬件信息放到 0x90000～0x901ff 里边
-! 嗯…毫不留情地将 bootsec 给覆盖掉了
-! 具体细节就无需读了，都是各种 BIOS 提供的服务
-! 各个地址上边放的啥我就懒得记了，见《Linux内核完全注释》P44 表3-3
+# 下边这一系列类似的代码，作用是将一些硬件信息放到 0x90000～0x901ff 里边
+# 嗯…毫不留情地将 bootsec 给覆盖掉了
+# 具体细节就无需读了，都是各种 BIOS 提供的服务
+# 各个地址上边放的啥我就懒得记了，见《Linux内核完全注释》P44 表3-3
 
-! ok, the read went well so we get current cursor position and save it for
-! posterity.
+# ok, the read went well so we get current cursor position and save it for
+# posterity.
 
-	mov	ax,#INITSEG	! this is done in bootsect already, but...
+	# this is done in bootsect already, but...
+	mov	ax,#INITSEG
 	mov	ds,ax
-	mov	ah,#0x03	! read cursor pos
+	# read cursor pos
+	mov	ah,#0x03
 	xor	bh,bh
-	int	0x10		! save it in known place, con_init fetches
-	mov	[0],dx		! it from 0x90000.
+	# save it in known place, con_init fetches
+	int	0x10
+	# it from 0x90000.
+	mov	[0],dx
 
-! Get memory size (extended mem, kB)
+# Get memory size (extended mem, kB)
 
 	mov	ah,#0x88
 	int	0x15
 	mov	[2],ax
 
-! Get video-card data:
+# Get video-card data:
 
 	mov	ah,#0x0f
 	int	0x10
-	mov	[4],bx		! bh = display page
-	mov	[6],ax		! al = video mode, ah = window width
+	# bh = display page
+	mov	[4],bx
+	# al = video mode, ah = window width
+	mov	[6],ax
 
-! check for EGA/VGA and some config parameters
+# check for EGA/VGA and some config parameters
 
 	mov	ah,#0x12
 	mov	bl,#0x10
@@ -73,7 +82,7 @@ start:
 	mov	[10],bx
 	mov	[12],cx
 
-! Get hd0 data
+# Get hd0 data
 
 	mov	ax,#0x0000
 	mov	ds,ax
@@ -85,7 +94,7 @@ start:
 	rep
 	movsb
 
-! Get hd1 data
+# Get hd1 data
 
 	mov	ax,#0x0000
 	mov	ds,ax
@@ -97,7 +106,7 @@ start:
 	rep
 	movsb
 
-! Check that there IS a hd1 :-)
+# Check that there IS a hd1 :-)
 
 	mov	ax,#0x01500
 	mov	dl,#0x81
@@ -115,218 +124,254 @@ no_disk1:
 	stosb
 is_disk1:
 
-! now we want to move to protected mode ...
+# now we want to move to protected mode ...
 
-	cli			! no interrupts allowed !
-	! 关中断
+	# no interrupts allowed !
+	cli
+	# 关中断
 
-! first we move the system to it's rightful place
+# first we move the system to it's rightful place
 
-! 先将 system 模块从 0x10000 挪到 0x00000 处
-! 这样 system 模块里边的绝对地址就能直接对得上内存地址了
+# 先将 system 模块从 0x10000 挪到 0x00000 处
+# 这样 system 模块里边的绝对地址就能直接对得上内存地址了
 
-! Q: 为什么不从一开始就在 bootsect 里把 system 载入到 0x00000 处呢？
-! A: 因为在实模式中，内存起始处的 1KiB 空间会用来存放中断向量表
-!    所以刚开始不能对其进行覆盖
+# Q: 为什么不从一开始就在 bootsect 里把 system 载入到 0x00000 处呢？
+# A: 因为在实模式中，内存起始处的 1KiB 空间会用来存放中断向量表
+#    所以刚开始不能对其进行覆盖
 
-! Q: 那为什么现在就可以覆盖到 0x00000 处呢？
-! A: 1. 这里已经关了中断响应，覆盖掉也没关系
-!    2. IDTR 在这期间也已经通过 lidt 指令完成了（暂时的）修改
-!       不过因为 IDTR 还没准备好，所以还不能开中断
+# Q: 那为什么现在就可以覆盖到 0x00000 处呢？
+# A: 1. 这里已经关了中断响应，覆盖掉也没关系
+#    2. IDTR 在这期间也已经通过 lidt 指令完成了（暂时的）修改
+#       不过因为 IDTR 还没准备好，所以还不能开中断
 
 	mov	ax,#0x0000
-	cld				! 'direction'=0, movs moves forward
-	! TODO: 不清楚 cld 指令在这的具体用途
+	# 'direction'=0, movs moves forward
+	cld
+	# TODO: 不清楚 cld 指令在这的具体用途
 do_move:
-	mov	es,ax		! destination segment
-	! 目标段为 0x0000~0x8000，步进 0x1000
+	# destination segment
+	mov	es,ax
+	# 目标段为 0x0000~0x8000，步进 0x1000
 	add	ax,#0x1000
 	cmp	ax,#0x9000
 	jz	end_move
-	mov	ds,ax		! source segment
+	# source segment
+	mov	ds,ax
 	sub	di,di
 	sub	si,si
 	mov 	cx,#0x8000
 	rep
 	movsw
-	! 一次移动一个字，所以一个循环就是移动了 0x10000 字节，刚好符合段长的步进值
+	# 一次移动一个字，所以一个循环就是移动了 0x10000 字节，刚好符合段长的步进值
 	jmp	do_move
 
-! then we load the segment descriptors
+# then we load the segment descriptors
 
 end_move:
-	mov	ax,#SETUPSEG	! right, forgot this at first. didn't work :-)
+	# right, forgot this at first. didn't work :-)
+	mov	ax,#SETUPSEG
 	mov	ds,ax
-	lidt	idt_48		! load idt with 0,0
-	lgdt	gdt_48		! load gdt with whatever appropriate
-	! 现在还在实模式下，但是这俩指令在实模式下也是可以执行的
-	! 注意前文挪动的是 system 模块，setup 模块还在原地(0x90200)
+	# load idt with 0,0
+	lidt	idt_48
+	# load gdt with whatever appropriate
+	lgdt	gdt_48
+	# 现在还在实模式下，但是这俩指令在实模式下也是可以执行的
+	# 注意前文挪动的是 system 模块，setup 模块还在原地(0x90200)
 
-! that was painless, now we enable A20
+# that was painless, now we enable A20
 
 	call	empty_8042
-	mov	al,#0xD1		! command write
+	# command write
+	mov	al,#0xD1
 	out	#0x64,al
 	call	empty_8042
-	mov	al,#0xDF		! A20 on
+	# A20 on
+	mov	al,#0xDF
 	out	#0x60,al
 	call	empty_8042
 
-! well, that went ok, I hope. Now we have to reprogram the interrupts :-(
-! we put them right after the intel-reserved hardware interrupts, at
-! int 0x20-0x2F. There they won't mess up anything. Sadly IBM really
-! messed this up with the original PC, and they haven't been able to
-! rectify it afterwards. Thus the bios puts interrupts at 0x08-0x0f,
-! which is used for the internal hardware interrupts as well. We just
-! have to reprogram the 8259's, and it isn't fun.
+# well, that went ok, I hope. Now we have to reprogram the interrupts :-(
+# we put them right after the intel-reserved hardware interrupts, at
+# int 0x20-0x2F. There they won't mess up anything. Sadly IBM really
+# messed this up with the original PC, and they haven't been able to
+# rectify it afterwards. Thus the bios puts interrupts at 0x08-0x0f,
+# which is used for the internal hardware interrupts as well. We just
+# have to reprogram the 8259's, and it isn't fun.
 
-! 不想读，这段弄懂了应该也没啥帮助，跟中断有关，应该是老设备的历史包袱，跳过
-
-	mov	al,#0x11		! initialization sequence
-	out	#0x20,al		! send it to 8259A-1
-	.word	0x00eb,0x00eb		! jmp $+2, jmp $+2
-	out	#0xA0,al		! and to 8259A-2
+# 不想读，这段弄懂了应该也没啥帮助，跟中断有关，应该是老设备的历史包袱，跳过
+    # initialization sequence
+	mov	al,#0x11
+	# send it to 8259A-1
+	out	#0x20,al
+	# jmp $+2, jmp $+2
 	.word	0x00eb,0x00eb
-	mov	al,#0x20		! start of hardware int's (0x20)
+	# and to 8259A-2
+	out	#0xA0,al
+	.word	0x00eb,0x00eb
+	# start of hardware int's (0x20)
+	mov	al,#0x20
 	out	#0x21,al
 	.word	0x00eb,0x00eb
-	mov	al,#0x28		! start of hardware int's 2 (0x28)
+	# start of hardware int's 2 (0x28)
+	mov	al,#0x28
 	out	#0xA1,al
 	.word	0x00eb,0x00eb
-	mov	al,#0x04		! 8259-1 is master
+	# 8259-1 is master
+	mov	al,#0x04
 	out	#0x21,al
 	.word	0x00eb,0x00eb
-	mov	al,#0x02		! 8259-2 is slave
+	# 8259-2 is slave
+	mov	al,#0x02
 	out	#0xA1,al
 	.word	0x00eb,0x00eb
-	mov	al,#0x01		! 8086 mode for both
+	# 8086 mode for both
+	mov	al,#0x01
 	out	#0x21,al
 	.word	0x00eb,0x00eb
 	out	#0xA1,al
 	.word	0x00eb,0x00eb
-	mov	al,#0xFF		! mask off all interrupts for now
+	# mask off all interrupts for now
+	mov	al,#0xFF
 	out	#0x21,al
 	.word	0x00eb,0x00eb
 	out	#0xA1,al
 
-! well, that certainly wasn't fun :-(. Hopefully it works, and we don't
-! need no steenking BIOS anyway (except for the initial loading :-).
-! The BIOS-routine wants lots of unnecessary data, and it's less
-! "interesting" anyway. This is how REAL programmers do it.
-!
-! Well, now's the time to actually move into protected mode. To make
-! things as simple as possible, we do no register set-up or anything,
-! we let the gnu-compiled 32-bit programs do that. We just jump to
-! absolute address 0x00000, in 32-bit protected mode.
-! 这里要准备进入保护模式了
+# well, that certainly wasn't fun :-(. Hopefully it works, and we don't
+# need no steenking BIOS anyway (except for the initial loading :-).
+# The BIOS-routine wants lots of unnecessary data, and it's less
+# "interesting" anyway. This is how REAL programmers do it.
+# 
+# Well, now's the time to actually move into protected mode. To make
+# things as simple as possible, we do no register set-up or anything,
+# we let the gnu-compiled 32-bit programs do that. We just jump to
+# absolute address 0x00000, in 32-bit protected mode.
+# 这里要准备进入保护模式了
 
-	mov	ax,#0x0001	! protected mode (PE) bit
-	lmsw	ax		! This is it!
-	! load machine status word
-	! 这条指令可被 "mov cr0, eax" 所（类似地）替代
+    # protected mode (PE) bit
+	mov	ax,#0x0001
+	# This is it!
+	lmsw	ax
+	# load machine status word
+	# 这条指令可被 "mov cr0, eax" 所（类似地）替代
 
-	! 这里有一个很好的问题：
-	! 既然在这条指令之后，CPU 就已经进入了保护模式，
-	! 那此时的 CS:EIP 就会因为 GDT 还没为此准备好，而指向错误的地址，
-	! 无法指向下一条指令，即 "jmpi 0, 8"
-	! 但是，事实上，这件事并不会发生，为什么？
+	# 这里有一个很好的问题：
+	# 既然在这条指令之后，CPU 就已经进入了保护模式，
+	# 那此时的 CS:EIP 就会因为 GDT 还没为此准备好，而指向错误的地址，
+	# 无法指向下一条指令，即 "jmpi 0, 8"
+	# 但是，事实上，这件事并不会发生，为什么？
 
-	! 因为当段寄存器被"载入"(即存入数据)时，CPU 才会去读取相应的段描述符
-	! 平时则是直接使用缓存
+	# 因为当段寄存器被"载入"(即存入数据)时，CPU 才会去读取相应的段描述符
+	# 平时则是直接使用缓存
 
-	! 来源：https://stackoverflow.com/questions/30932302/i-am-confusing-some-assembly-code-about-enable-pe-within-boot-setup-s-file-in-linux-0-11
+	# 来源：https://stackoverflow.com/questions/30932302/i-am-confusing-some-assembly-code-about-enable-pe-within-boot-setup-s-file-in-linux-0-11
+	# jmp offset 0 of segment 8 (cs)
+	jmpi	0,8
 
-	jmpi	0,8		! jmp offset 0 of segment 8 (cs)
+	# 针对上边这条指令，我再补一些补充说明，不翻译了，反正不难懂
+	# 来源：http://www.oldlinux.org/Linux.old/study/eclk-03-boot.pdf
 
-	! 针对上边这条指令，我再补一些补充说明，不翻译了，反正不难懂
-	! 来源：http://www.oldlinux.org/Linux.old/study/eclk-03-boot.pdf
+	# As we have moved the system module begin at the absolute address 0x0000, the jump
+	# instruction above will pass the CPU controls to the head.s code at the begin of
+	# system module. The segment selector ( 8 ) is used to select a segment descripter
+	# in an identified descripter table. Segment selector has 16 bits, bit 0-1 represents
+	# requested privilege level (RPL), bit 2 is a table indicator used to specify the
+	# refered descriptor table ( 0 - GDT, 1- LDT), bit 3-15 is INDEX field used to index
+	# a descripter in the talbe. A sepecified descripter address is:
+	# Table Base Address + INDEX * 8
+	# So segment selector 8 (0b0000,0000,0000,1000) means RPL=0, selects item 1 (INDEX=1)
+	# within globle descripter table (GDT). This specified descripter indicates that the
+	# segment base address is 0 (see L209), so the instruction of L193 will jump to the
+	# beginning of system module.
 
-	!! As we have moved the system module begin at the absolute address 0x0000, the jump
-	!! instruction above will pass the CPU controls to the head.s code at the begin of
-	!! system module. The segment selector ( 8 ) is used to select a segment descripter
-	!! in an identified descripter table. Segment selector has 16 bits, bit 0-1 represents
-	!! requested privilege level (RPL), bit 2 is a table indicator used to specify the
-	!! refered descriptor table ( 0 - GDT, 1- LDT), bit 3-15 is INDEX field used to index
-	!! a descripter in the talbe. A sepecified descripter address is:
-	!! Table Base Address + INDEX * 8
-	!! So segment selector 8 (0b0000,0000,0000,1000) means RPL=0, selects item 1 (INDEX=1)
-	!! within globle descripter table (GDT). This specified descripter indicates that the
-	!! segment base address is 0 (see L209), so the instruction of L193 will jump to the
-	!! beginning of system module.
+	# 大体意思就是，这个 8 是一个段选择子，最终效果是跳到 GDT 第一项对应的内存空间（且 offset=0）
+	# 且 GDT 的第一项描述符，其基址被设置为了 0
+	# 加上 system 模块已经被挪到了 0x00000 处
+	# 最终效果就是跳到 system 模块去啦（入口是 boot/head.s）
 
-	! 大体意思就是，这个 8 是一个段选择子，最终效果是跳到 GDT 第一项对应的内存空间（且 offset=0）
-	! 且 GDT 的第一项描述符，其基址被设置为了 0
-	! 加上 system 模块已经被挪到了 0x00000 处
-	! 最终效果就是跳到 system 模块去啦（入口是 boot/head.s）
+	# 关于段选择子：
 
-	! 关于段选择子：
+	# |15    3|  2  |1 0|
+	# +-----------------+
+	# | Index | G/L |RPL|
+	# +-----------------+
 
-	! |15    3|  2  |1 0|
-	! +-----------------+
-	! | Index | G/L |RPL|
-	! +-----------------+
+	# G/L: 0 - GDT, 1 - LDT
 
-	! G/L: 0 - GDT, 1 - LDT
-
-! This routine checks that the keyboard command queue is empty
-! No timeout is used - if this hangs there is something wrong with
-! the machine, and we probably couldn't proceed anyway.
+# This routine checks that the keyboard command queue is empty
+# No timeout is used - if this hangs there is something wrong with
+# the machine, and we probably couldn't proceed anyway.
 empty_8042:
 	.word	0x00eb,0x00eb
-	in	al,#0x64	! 8042 status port
-	test	al,#2		! is input buffer full?
-	jnz	empty_8042	! yes - loop
+	# 8042 status port
+	in	al,#0x64
+	# is input buffer full?
+	test	al,#2
+	# yes - loop
+	jnz	empty_8042
 	ret
 
-! GDT 描述符结构：
+# GDT 描述符结构：
 
-! |31                                          16|15                              0|
-! +----------------------------------------------+---------------------------------+
-! |                   Base 15~0                  |            Limit 15~0           |
-! +--------------------------------------------------------------------------------+
+# |31                                          16|15                              0|
+# +----------------------------------------------+---------------------------------+
+# |                   Base 15~0                  |            Limit 15~0           |
+# +--------------------------------------------------------------------------------+
 
-! |31        24| 23|  22 | 21|  20 |19         16| 15|14 13| 12|11   8|7          0|
-! +--------------------------------------------------------------------------------+
-! | Base 31~24 | G | D/B | L | AVL | Limit 19~16 | P | DPL | S | TYPE | Base 23~16 |
-! +--------------------------------------------------------------------------------+
+# |31        24| 23|  22 | 21|  20 |19         16| 15|14 13| 12|11   8|7          0|
+# +--------------------------------------------------------------------------------+
+# | Base 31~24 | G | D/B | L | AVL | Limit 19~16 | P | DPL | S | TYPE | Base 23~16 |
+# +--------------------------------------------------------------------------------+
 
-! G: 	粒度
-! D/B:	默认的操作数大小
-! L:	标示处理器类型(32/64位)
-! AVL:	供操作系统使用
+# G: 	粒度
+# D/B:	默认的操作数大小
+# L:	标示处理器类型(32/64位)
+# AVL:	供操作系统使用
 
-! P:	存在位
-! DPL:	特权级
-! S:	描述符类型
-!		S=0: 这是一个系统段
-!		S=1: 这是一个代码段/数据段
-! TYPE: X E W A (X=0, 数据)
-!		X C R A (X=1, 代码)
+# P:	存在位
+# DPL:	特权级
+# S:	描述符类型
+# 		S=0: 这是一个系统段
+# 		S=1: 这是一个代码段/数据段
+# TYPE: X E W A (X=0, 数据)
+# 		X C R A (X=1, 代码)
 
 gdt:
-	.word	0,0,0,0		! dummy
-	! 第一个描述符必须是全零 (CPU 的特殊规定)
+	# dummy
+	.word	0,0,0,0
+	# 第一个描述符必须是全零 (CPU 的特殊规定)
 
-	.word	0x07FF		! 8Mb - limit=2047 (2048*4096=8Mb)
-	.word	0x0000		! base address=0
-	.word	0x9A00		! code read/exec,  "1 00 1 1010 00000000"
-	.word	0x00C0		! granularity=4096, 386
+	# 8Mb - limit=2047 (2048*4096=8Mb)
+	.word	0x07FF
+	# base address=0
+	.word	0x0000
+	# code read/exec,  "1 00 1 1010 00000000"
+	.word	0x9A00
+	# granularity=4096, 386
+	.word	0x00C0
 
-	.word	0x07FF		! 8Mb - limit=2047 (2048*4096=8Mb)
-	.word	0x0000		! base address=0
-	.word	0x9200		! data read/write, "1 00 1 0010 00000000"
-	.word	0x00C0		! granularity=4096, 386
+	# 8Mb - limit=2047 (2048*4096=8Mb)
+	.word	0x07FF
+	# base address=0
+	.word	0x0000
+	# data read/write, "1 00 1 0010 00000000"
+	.word	0x9200
+	# granularity=4096, 386
+	.word	0x00C0
 
 
 idt_48:
-	.word	0			! idt limit=0
-	.word	0,0			! idt base=0L
-! lidt 指令的操作数
+	# idt limit=0
+	.word	0
+	# idt base=0L
+	.word	0,0
+# lidt 指令的操作数
 
 gdt_48:
-	.word	0x800		! gdt limit=2048, 256 GDT entries
-	.word	512+gdt,0x9	! gdt base = 0x9XXXX
-! lgdt 指令的操作数
+	# gdt limit=2048, 256 GDT entries
+	.word	0x800
+	# gdt base = 0x9XXXX
+	.word	512+gdt,0x9
+# lgdt 指令的操作数
 	
 .text
 endtext:
